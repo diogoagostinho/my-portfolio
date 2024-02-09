d5c77e207f5205e1f5b86db016593a5c2742e0aa
AGOSTINHO <LaaGostim.OFICIAL@gmail.com>
2024-02-09 12:30:50 +0000

    0.0.4
    Added footer, updated

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,8 @@
+function Accordion() {
+  return (
+    <>
+      <h1>Accordion</h1>
+    </>
+  );
+}
+export default Accordion;
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,7 +1,72 @@
+import Footer from "../components/Footer";
+import NavigationButton from "../components/NavigationButton";
+import "../styles/work.scss";
+
 function Work() {
   return (
     <>
-      <h1>Work</h1>
+      <div className="work">
+        <div className="work__content">
+          <div className="work-left">
+            <div className="work__header">
+              <h1>Portfolio</h1>
+              <h2>Have a look at all my projects</h2>
+            </div>
+            <h1>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Molestias doloribus ducimus porro quam officia aspernatur illum
+              necessitatibus, amet, sequi deserunt quas voluptates saepe, odio
+              tempore itaque voluptatum at. Quos, eveniet.
+            </h1>
+            <h1>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Molestias doloribus ducimus porro quam officia aspernatur illum
+              necessitatibus, amet, sequi deserunt quas voluptates saepe, odio
+              tempore itaque voluptatum at. Quos, eveniet.
+            </h1>
+            <h1>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Molestias doloribus ducimus porro quam officia aspernatur illum
+              necessitatibus, amet, sequi deserunt quas voluptates saepe, odio
+              tempore itaque voluptatum at. Quos, eveniet.
+            </h1>
+            <h1>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Molestias doloribus ducimus porro quam officia aspernatur illum
+              necessitatibus, amet, sequi deserunt quas voluptates saepe, odio
+              tempore itaque voluptatum at. Quos, eveniet.
+            </h1>
+            <div className="work__section"></div>
+          </div>
+          <div className="work-right">
+            <NavigationButton
+              title={"ABOUT ME"}
+              desc={"Learn all about me!"}
+              link={"/"}
+              active={false}
+            />
+            <NavigationButton
+              title={"WORK"}
+              desc={"View my portfolio"}
+              link={"work"}
+              active={true}
+            />
+            <NavigationButton
+              title={"RESUME"}
+              desc={"My studies and certifications"}
+              link={"resume"}
+              active={false}
+            />
+            <NavigationButton
+              title={"EXPERIENCE"}
+              desc={"Everything I know"}
+              link={"experience"}
+              active={false}
+            />
+          </div>
+        </div>
+        <Footer />
+      </div>
     </>
   );
 }
